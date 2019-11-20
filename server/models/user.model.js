e654565e619e361a859e475272ca070f648353a5
roger <rogermuhire@gmail.com>
2019-11-20 18:05:38 +0200

    ch(endpoints):create user account created user account [finishes #169842977]

---
+++ server/models/user.model.js
@@ -0,0 +1,12 @@
+class User {
+    constructor(id, firstname, lastname, email, password, PhoneNumber, username) {
+      this.id = id;
+      this.firstname = firstname;
+      this.lastname = lastname;
+      this.email = email;
+      this.password = password;
+      this.PhoneNumber = PhoneNumber;
+      this.username = username;
+    }
+  }
+  export default User;
